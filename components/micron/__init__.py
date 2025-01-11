aa4055f6cec9b7ef0f80c2619e8a3e1e5ce400d8
schneemaier <akos.schneemaier@gmail.com>
2025-01-10 19:53:30 -0500

    adding siren pins

--- components/micron/__init__.py
+++ components/micron/__init__.py
@@ -167,10 +167,10 @@ async def to_code(config):
     cg.add(var.set_pin_data(pin_data))
     pin_data_out = await gpio_pin_expression(config[CONF_DATA_OUT_PIN])
     cg.add(var.set_pin_data_out(pin_data_out))
-    siren_data = await gpio_pin_expression(config[CONF_SIREN_IN_PIN])
-    cg.add(var.set_pin_data(siren_data))
-    siren_data_out = await gpio_pin_expression(config[CONF_SIREN_OUT_PIN])
-    cg.add(var.set_pin_data_out(siren_data_out))
+    pin_siren = await gpio_pin_expression(config[CONF_SIREN_IN_PIN])
+    cg.add(var.set_pin_data(pin_siren))
+    pin_siren_out = await gpio_pin_expression(config[CONF_SIREN_OUT_PIN])
+    cg.add(var.set_pin_data_out(pin_siren_out))
 
     if CONF_CONNECTED in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_CONNECTED])
