6fe47801dcd7482fbd01f5c44675cbbe91752b2e
schneemaier <akos.schneemaier@gmail.com>
2025-08-17 09:31:24 -0400

    1111

--- components/micron/micron.cpp
+++ components/micron/micron.cpp
@@ -198,7 +198,7 @@ namespace esphome
       // write command
       arg->processor_.write(&arg->pin_data_out_);
       bool data_bit = arg->pin_data_.digital_read();
-      ESP_LOGD(TAG, "PAcketBits: %d, Data: %d", arg->packet_bits, data_bit);
+      //ESP_LOGD(TAG, "PAcketBits: %d, Data: %d", arg->packet_bits, data_bit);
       if (arg->processor_.decode(now_ms, data_bit, arg->frame_size)) {
         ESP_LOGD(TAG, "DECODE RETURNED TRUE");
         arg->last_packet_ms = now_ms;
