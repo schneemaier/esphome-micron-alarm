5aa50d74c61ed2801645cd569bed14ac7550e983
schneemaier <akos.schneemaier@gmail.com>
2025-09-03 18:20:42 -0400

    testtest

--- components/micron/__init__.py
+++ components/micron/__init__.py
@@ -11,7 +11,6 @@ from esphome.cpp_helpers import gpio_pin_expression
 from esphome.const import (
     CONF_ID,
     CONF_CLOCK_PIN,
-    CONF_CLOCK_PIN2,
     CONF_DATA_PIN,
     DEVICE_CLASS_OCCUPANCY,
     DEVICE_CLASS_POWER,
@@ -34,6 +33,7 @@ MicronComponent = micron_ns.class_(
 
 MicronPressAction = micron_ns.class_("MicronPressAction", automation.Action)
 
+CONF_CLOCK2_PIN = "clock2_pin"
 CONF_DATA_IN_PIN = "data_in_pin"
 CONF_DATA_OUT_PIN = "data_out_pin"
 CONF_SIREN_IN_PIN = "siren_in_pin"
@@ -72,7 +72,7 @@ CONFIG_SCHEMA = cv.All(
         {
             cv.GenerateID(): cv.declare_id(MicronComponent),
             cv.Required(CONF_CLOCK_PIN): cv.All(pins.internal_gpio_input_pin_schema),
-            cv.Required(CONF_CLOCK_PIN2): cv.All(pins.internal_gpio_input_pin_schema),
+            cv.Required(CONF_CLOCK2_PIN): cv.All(pins.internal_gpio_input_pin_schema),
             cv.Required(CONF_DATA_IN_PIN): cv.All(pins.internal_gpio_input_pin_schema),
             cv.Required(CONF_DATA_OUT_PIN): cv.All(pins.internal_gpio_output_pin_schema),
             cv.Required(CONF_SIREN_IN_PIN): cv.All(pins.internal_gpio_input_pin_schema),
@@ -170,8 +170,8 @@ async def to_code(config):
 
     pin_clock = await gpio_pin_expression(config[CONF_CLOCK_PIN])
     cg.add(var.set_pin_clock(pin_clock))
-    pin_clock2 = await gpio_pin_expression(config[CONF_CLOCK_PIN2])
-    cg.add(var.set_pin_clock(pin_clock2))
+    pin_clock2 = await gpio_pin_expression(config[CONF_CLOCK2_PIN])
+    cg.add(var.set_pin_clock2(pin_clock2))
     pin_data = await gpio_pin_expression(config[CONF_DATA_IN_PIN])
     cg.add(var.set_pin_data(pin_data))
     pin_data_out = await gpio_pin_expression(config[CONF_DATA_OUT_PIN])
