cd523554d50a4c8f501c7fd6e150296a2764d875
schneemaier <akos.schneemaier@gmail.com>
2026-09-08 16:26:53 -0400

    update

--- components/micron/micron.h
+++ components/micron/micron.h
@@ -5,7 +5,7 @@
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/core/hal.h"
 #include "esphome/core/helpers.h"
-#include "queue.h"
+#include <queue>
 
 namespace esphome
 {
